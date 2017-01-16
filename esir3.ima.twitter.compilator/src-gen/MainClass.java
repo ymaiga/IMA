8b02fa7c7b970845a74205856bf2118adff57a21
Ali Mohamed <Ali Mohamed>
2017-01-16 21:50:30 +0100

    debut de génération de code

---
+++ esir3.ima.twitter.compilator/src-gen/MainClass.java
@@ -0,0 +1,15 @@
+public class MainClass{
+	***visite prog
+	public static void main(String[] args){
+			
+	 			List<String> args_1 = new ArryList<String>();
+				//debut itérationimaModel2.impl.UserImpl@27305e6 (name: Ali)
+list.add(Ali);nullnull
+			
+	 			List<String> args_0 = new ArryList<String>();
+				//debut itérationimaModel2.impl.UserImpl@27305e6 (name: Ali)
+list.add(Ali);null
+	
+	}
+
+}
