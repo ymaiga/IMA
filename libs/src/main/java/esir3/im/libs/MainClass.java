98f602b0026a8dda382f5849a51bce2fc4665d78
Ali mohamed <alimohamedaffizou@gmail.com>
2017-01-26 23:23:14 +0100

    set generate folder to lib

---
+++ libs/src/main/java/esir3/im/libs/MainClass.java
@@ -0,0 +1,36 @@
+package esir3.im.libs;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+public class MainClass{
+
+	public static void main(String[] args){
+	LibTw libTw = new LibTw();
+			
+	 			List<String> args_0 = new ArrayList<String>();
+				//debut parcours du chemin0 (id = 0)
+args_0.add("@Ali");
+args_0.add("!newHashtag");
+if (libTw.count(args_0) >20){
+libTw.plot(args_0);
+}
+ else{
+System.out.println(libTw.getTweets(args_0));
+}
+System.out.println( libTw.count(args_0) );
+
+			
+	 			List<String> args_1 = new ArrayList<String>();
+				//debut parcours du chemin1 (id = 1)
+args_1.add("#ESIR");
+libTw.getPropPositif(args_1);
+System.out.println(libTw.getTweets(args_1));
+System.out.println( libTw.count(args_1) );
+
+	
+	}
+
+}
